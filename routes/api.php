e195954dc7f840740040a5c0ffd0052ad9a58e88
Vladislav Mischenko <vamischenko0@gmail.com>
2018-11-29 18:19:25 +0300

    update api.php

--- routes/api.php
+++ routes/api.php
@@ -19,3 +19,27 @@ Route::post('/comments/{id}', 'CommentsController@store');
 Route::put('/comments/{id}', 'CommentsController@update');
 Route::delete('/comments/{id}', 'CommentsController@destroy');
 
+//Маршруты для Категорий
+Route::get('/categories', 'CategoriesController@index');
+Route::get('/categories/{category}','CategoriesController@show');
+Route::get('categories/create', 'CategoriesController@create');
+Route::post('/categories', 'CategoriesController@store');
+Route::put('/categories/{category}', 'CategoriesController@update');
+Route::get('categories/{category}/edit', 'CategoriesController@edit');
+Route::delete('/categories/{category}', 'CategoriesController@destroy');
+//Маршруты для Тегов
+Route::get('/tags','TagsController@index');
+Route::get('/tags/{tag}','TagsController@show');
+Route::get('tags/create','TagsController@create');
+Route::post('/tags', 'TagsController@store');
+Route::patch('/tags/{tag}', 'TagsController@update');
+Route::get('tags/{tag}/edit','TagsController@edit');
+Route::delete('/tags/{tag}', 'TagsController@destroy');
+//Маршруты для Пользователя
+Route::get('/users', 'UsersController@index');
+Route::get('/users/{user}','UsersController@show');
+Route::get('users/create', 'UsersController@create');
+Route::post('/users', 'UsersController@store');
+Route::put('/users/{user}', 'UsersController@update');
+Route::get('users/{user}/edit', 'UsersController@edit');
+Route::delete('/users/{user}', 'UsersController@destroy');
