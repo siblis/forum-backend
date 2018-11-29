f2a55415d45ae9e92cad78edcc7a8ca189c15e23
Vladislav Mischenko <vamischenko0@gmail.com>
2018-11-29 18:10:23 +0300

    update api.php

--- routes/api.php
+++ routes/api.php
@@ -4,17 +4,6 @@ header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE')
 header( 'Access-Control-Allow-Headers', 'X-Requested-With, Authorization, Content-Type' );
 use Illuminate\Http\Request;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
