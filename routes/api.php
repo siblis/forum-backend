2676e9a70a370d7237c899b8aed5fd8c608282ca
Anatoly_Chernyshev <kayn23@yandex.ru>
2018-12-08 17:49:50 +0300

    Настройка проверки авторизации. Теперь для создания и редактирования надо отправлять заголовок с токеном

--- routes/api.php
+++ routes/api.php
@@ -7,37 +7,23 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
-
+//Маршруты для постов
 Route::get('/posts', 'PostsController@index');
 Route::get('/post/{post}', 'PostsController@show');
-Route::post('/posts', 'PostsController@store');
-Route::put('/posts/{post}', 'PostsController@update');
-Route::delete('/posts/{post}', 'PostsController@delete');
-
+//Маршруты для комментариев
 Route::get('/comments/{id}', 'CommentsController@index');
-Route::post('/comments/{id}', 'CommentsController@store');
-Route::put('/comments/{id}', 'CommentsController@update');
-Route::delete('/comments/{id}', 'CommentsController@destroy');
-
 //Маршруты для Категорий
 Route::get('/categories', 'CategoriesController@index');
 Route::get('/categories/{category}','CategoriesController@show');
-Route::post('/categories', 'CategoriesController@store');
-Route::put('/categories/{category}', 'CategoriesController@update');
-Route::delete('/categories/{category}', 'CategoriesController@destroy');
 //Маршруты для Тегов
 Route::get('/tags','TagsController@index');
 Route::get('/tags/{tag}','TagsController@show');
-Route::post('/tags', 'TagsController@store');
-Route::patch('/tags/{tag}', 'TagsController@update');
-Route::delete('/tags/{tag}', 'TagsController@destroy');
+
 
 //Маршруты для Пользователя
 Route::group([
-
     'middleware' => ['api','cors'],
     'prefix' => 'users'
-
 ], function ($router) {
 
     Route::post('login', 'AuthController@login'); //Авторизация пользователя. Возвращает user и token
@@ -47,5 +33,22 @@ Route::group([
     Route::post('register', 'AuthController@register'); //Регистрация пользователя. Возвращает user, token, статус 201
 });
 
-//В фаиле api.php указываеются роуты именно для создание api приложения.
-//полный адресс будет выглядить примерно так mysait.com/api/post или mysait.com/api/user
+//
+Route::group(['middleware' => ['jwt.verify']], function(){
+    //роуты для постов
+    Route::post('/posts', 'PostsController@store');
+    Route::put('/posts/{post}', 'PostsController@update');
+    Route::delete('/posts/{post}', 'PostsController@delete');
+    //роуты для комментариев
+    Route::post('/comments/{id}', 'CommentsController@store');
+    Route::put('/comments/{id}', 'CommentsController@update');
+    Route::delete('/comments/{id}', 'CommentsController@destroy');
+    //роуты для тегов
+    Route::post('/tags', 'TagsController@store');
+    Route::patch('/tags/{tag}', 'TagsController@update');
+    Route::delete('/tags/{tag}', 'TagsController@destroy');
+    //роуты для категорий
+    Route::post('/categories', 'CategoriesController@store');
+    Route::put('/categories/{category}', 'CategoriesController@update');
+    Route::delete('/categories/{category}', 'CategoriesController@destroy');
+});
