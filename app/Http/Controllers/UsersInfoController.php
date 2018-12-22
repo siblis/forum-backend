17ddaaf6fdbf9d5f6515c3d1c68cd44c16c43912
Anatoly Chernyshev <kayn23@yandex.ru>
2018-12-23 00:24:26 +0300

    fix user info, delete facades DB

--- app/Http/Controllers/UsersInfoController.php
+++ app/Http/Controllers/UsersInfoController.php
@@ -2,28 +2,29 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Support\Facades\DB;
+use App\UsersInfo;
 
 class UsersInfoController extends Controller
 {
 
     public function show($id)
     {
-        $user_info = DB::table('users_info')->where('id', $id)->get();
+        $user_info = UsersInfo::find($id);
         return response()->json($user_info, 200);
     }
 
     public function update($id)
     {
-        $user_info = DB::table('users_info')->where('id', $id)->update
-            ([
-                'avatar' => request('avatar'),
-                'full_name' => request('full_name'),
-                'phone' => request('avatar'),
-                'about' => request('avatar'),
-                'rating' => request('rating'),
-                'rule' => request('rule')
-            ]);
+        $user_info = UsersInfo::find($id);
+        $user_info->update
+        ([
+            'avatar' => request('avatar'),
+            'full_name' => request('full_name'),
+            'phone' => request('avatar'),
+            'about' => request('avatar'),
+            'rating' => request('rating'),
+            'rule' => request('rule')
+        ]);
 
         return response()->json($user_info, 200);
     }
